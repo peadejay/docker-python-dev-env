38ad70034d00c7aa52ec47848e4ecf7b1c4f76df
peadejay <peadejay@gmail.com>
2017-12-29 02:29:06 +0000

    add start script

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
 # docker_python_dev_env
 A Docker container to be used as Python 3.6 development environment.
 
-## Installation
-
-Build the image
+## Usage 
 ```
-docker build -t python_dev_env .
-```
-
-Start the container
-```
-docker run -it --rm --mount type=bind,source=<host path>,targer=<container path> <image name>
-
-# For example
-docker run -it --rm --mount type=bind,source="$(pwd)"/shared,target=/home/shared python_dev_env
+./start.sh <path to shared directory>
 ```
---
+++ start.sh
@@ -0,0 +1,25 @@
+if [ -z "$1" ]; then
+    echo "Usage: start.sh <path to shared directory>"
+    exit 1
+fi
+
+# Get the mount point and expand into absolute path
+MOUNT_POINT=$(readlink -f $1)
+echo "Using mount point: ${MOUNT_POINT}"
+
+# Ensure the mount point exists
+mkdir -p ${MOUNT_POINT}
+
+IMAGE_NAME="python_dev_env"
+
+if [[ "$(docker images -q ${IMAGE_NAME} 2> /dev/null)" == "" ]]; then
+    echo "Image does not exist - building now..."
+    docker build -t ${IMAGE_NAME} .
+fi
+
+echo "========================================"
+echo "Starting development container"
+echo "Shared directory located at /home/shared"
+echo "Press Ctrl-d to exit..."
+echo "========================================"
+docker run -it --rm --mount type=bind,source=${MOUNT_POINT},target=/home/shared ${IMAGE_NAME}
